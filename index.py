9efbad52da4f14cd49e79ce0fcb22bf81e306409
shoh99 <46786219+shoh99@users.noreply.github.com>
2021-05-30 21:39:18 +0900

    Update index.py

--- index.py
+++ index.py
@@ -55,7 +55,7 @@ st.write("""
     # Lets Explore different classification with Iris dataset
 """)
 # function to get dataset
-df = pd.read_csv('dataset\iris.csv')
+df = pd.read_csv('iris.csv')
 st.write(df)
 
 
